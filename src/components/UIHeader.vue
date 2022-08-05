944ba07d49dc571b69e56d944c4dfc87a9b53834
Diana <divnvp@yandex.com>
2022-08-05 19:46:21 +0300

    add: UIHeader.vue component

---
+++ src/components/UIHeader.vue
@@ -0,0 +1,21 @@
+<script setup>
+//
+import UINavigation from "./UINavigation.vue";
+</script>
+
+<template>
+  <section class="row justify-content__space-between align-items__center">
+    <label class="row align-items__center">
+      <span class="logo">G</span>
+      <span>Global</span>
+    </label>
+
+    <nav>
+      <UINavigation />
+    </nav>
+
+    <button class="button_sign-up">
+      Sign Up
+    </button>
+  </section>
+</template>
