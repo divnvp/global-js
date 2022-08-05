28e44a7ebd9e6f285e4a553ba01b8925ed124746
Diana <divnvp@yandex.com>
2022-08-05 19:46:10 +0300

    add: UIFooter.vue component

---
+++ src/components/UIFooter.vue
@@ -0,0 +1,31 @@
+<script setup>
+//
+</script>
+
+<template>
+  <section class="user-photos">
+    <img
+        src="../pictures/img.png"
+        alt="User photo"
+    >
+    <img
+        src="../pictures/img_1.png"
+        alt="User photo"
+        class="user-photos__two"
+    >
+    <img
+        src="../pictures/img_2.png"
+        alt="User photo"
+        class="user-photos__three"
+    >
+    <span class="user-photos__add">
+            <img src="../icons/plus.png" alt="Add icon">
+          </span>
+  </section>
+
+  <section class="row align-items__center">
+    <label class="user-photos__label_grey">45k+ Regular User.
+      <span class="user-photos__label_orange">View Price Plan</span>
+    </label>
+  </section>
+</template>
