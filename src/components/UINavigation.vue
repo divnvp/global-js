ad58364e8a455f189474b904ed62d2ab1c89e85d
Diana <divnvp@yandex.com>
2022-08-05 19:46:33 +0300

    add: UINavigation.vue component

---
+++ src/components/UINavigation.vue
@@ -0,0 +1,12 @@
+<script setup>
+//
+</script>
+
+<template>
+  <section class="navigation">
+    <span>Mobile Top Up</span>
+    <span>About</span>
+    <span>Rate</span>
+    <span>Help</span>
+  </section>
+</template>
