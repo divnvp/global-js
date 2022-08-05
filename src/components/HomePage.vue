cf43634f02bab6c89cd354d0fcf844dd90a1975f
Diana <divnvp@yandex.com>
2022-08-05 11:21:33 +0300

    add: HomePage.vue component with semantic tags

---
+++ src/components/HomePage.vue
@@ -0,0 +1,15 @@
+<script setup>
+//
+import UIHeader from "./UIHeader.vue";
+import UIMain from "./UIMain.vue";
+</script>
+
+<template>
+  <header>
+    <UIHeader />
+  </header>
+
+  <main>
+    <UIMain />
+  </main>
+</template>
